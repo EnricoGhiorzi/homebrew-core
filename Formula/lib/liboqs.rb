41107021a7a6de2f7ff306183701b19709ae265d
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2026-05-03 14:25:19 +0900

    liboqs: migrate to `openssl@4`

--- Formula/lib/liboqs.rb
+++ Formula/lib/liboqs.rb
@@ -22,12 +22,12 @@ class Liboqs < Formula
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "ninja" => :build
-  depends_on "openssl@3"
+  depends_on "openssl@4"
 
   def install
     args = %W[
       -DOQS_USE_OPENSSL=ON
-      -DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}
+      -DOPENSSL_ROOT_DIR=#{Formula["openssl@4"].opt_prefix}
     ]
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
@@ -40,8 +40,8 @@ class Liboqs < Formula
   test do
     cp pkgshare/"tests/example_kem.c", "test.c"
     system ENV.cc, "test.c",
-                  "-I#{Formula["openssl@3"].include}", "-I#{include}",
-                  "-L#{Formula["openssl@3"].lib}", "-L#{lib}",
+                  "-I#{Formula["openssl@4"].include}", "-I#{include}",
+                  "-L#{Formula["openssl@4"].lib}", "-L#{lib}",
                   "-loqs", "-lssl", "-lcrypto", "-o", "test"
     assert_match "operations completed", shell_output("./test")
   end
